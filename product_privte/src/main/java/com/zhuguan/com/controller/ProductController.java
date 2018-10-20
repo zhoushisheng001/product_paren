2d43e5f603faa3fcb69c89a63dd7c7eafe7d4aed
zhoushisheng <1358281371@qq.com>
2018-10-20 15:33:57 +0800

    商品服务12

--- product_privte/src/main/java/com/zhuguan/com/controller/ProductController.java
+++ product_privte/src/main/java/com/zhuguan/com/controller/ProductController.java
@@ -4,7 +4,10 @@ import com.zhuguan.com.service.ProductService;
 import com.zhuguan.zhou.api.ProductApi;
 import com.zhuguan.zhou.model.ProductDto;
 import com.zhuguan.zhou.response.ResponseData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,13 +17,19 @@ import java.util.List;
 @RestController
 public class ProductController implements ProductApi {
 
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private ProductService productService;
+
+    @Value("${server.port}")
+    private String port;
 
     @Override
     public ResponseData<ProductDto> getProductInfoById(@RequestParam("id") String id) {
         ResponseData<ProductDto> resp = new ResponseData<>();
         resp.setData(productService.getProductInfoById(id)).ok();
+        System.out.println("===========>port:" + port);
         return resp;
     }
 
@@ -28,6 +37,7 @@ public class ProductController implements ProductApi {
     public ResponseData<List<ProductDto>> getgetProductInfoList() {
         ResponseData<List<ProductDto>> resp = new ResponseData<>();
         resp.setData(productService.getgetProductInfoList()).ok();
+        logger.info("==================>port:" + port + "<====================");
         return resp;
     }
 }
